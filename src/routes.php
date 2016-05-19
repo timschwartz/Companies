813b8c6413ca66271cfdbdd75414f09ef464f069
Tim Schwartz <tim@metaverse.systems>
2016-05-18 19:52:47 -0500

    Fixed typo in routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(array('prefix'=>'api/v1', 'middleware' => ['web, auth']), function()
+Route::group(array('prefix'=>'api/v1', 'middleware' => ['web', 'auth']), function()
 {
     Route::resource('company', '\timschwartz\Companies\controllers\CompanyController');
 });
