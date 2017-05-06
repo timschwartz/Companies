2925c4fff16869af70f4a0b128e7265cb978704a
Tim Schwartz <tim@timscomputerservices.com>
2017-05-06 13:40:06 -0500

    Added employees()

---
+++ src/database/migrations/2017_05_06_100000_create_company_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timschwartz_company_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('company_id');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('timschwartz_company_user');
+    }
+}
+
--- src/models/Company.php
+++ src/models/Company.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $table = 'timschwartz_companies';
+
+    public function employees()
+    {
+        return $this->belongsToMany('App\User', 'timschwartz_company_user');
+    }
+
+    public function phones()
+    {
+        return $this->belongsToMany('timschwartz\Phone', 'timschwartz_company_phone');
+    }
+
 }
